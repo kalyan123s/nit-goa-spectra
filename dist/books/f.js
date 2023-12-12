c8e91fdf19d73763a30aca87cea7167b4a19ee90
yadavsk098 <yadavsk098@gmail.com>
2023-12-12 20:53:26 +0530

    minor changes

--- dist/books/f.js
+++ dist/books/f.js
@@ -1,40 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
     const pdfPath = 'sample.pdf'; // Replace with the path to your PDF file
-    const flipbookContainer = document.getElementById('flipbook');
-
-    // Initialize flip book
-    $(flipbookContainer).turn();
+    const canvas = document.getElementById('pdf-canvas');
+    const context = canvas.getContext('2d');
 
     // Initialize PDF.js
     pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
 
-    // Fetch PDF and convert it to flip book pages
-    pdfjsLib.getDocument(pdfPath).then(function (pdf) {
-        for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-            pdf.getPage(pageNum).then(function (page) {
-                const canvas = document.createElement('canvas');
-                const context = canvas.getContext('2d');
-                const viewport = page.getViewport({ scale: 1.5 });
+    // Fetch PDF and render it to the canvas
+    pdfjsLib.getDocument(pdfPath).promise.then(function (pdfDocument) {
+        // Load the first page of the PDF
+        pdfDocument.getPage(1).then(function (page) {
+            const viewport = page.getViewport({ scale: 1.5 });
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
 
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
+            // Render the PDF page to the canvas
+            const renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
 
-                const renderContext = {
-                    canvasContext: context,
-                    viewport: viewport
-                };
-
-                page.render(renderContext).promise.then(function () {
-                    const pageElement = $('<div class="pdf-page">').html('<img src="' + canvas.toDataURL() + '">');
-                    if (pageNum % 2 === 0) {
-                        pageElement.addClass('even');
-                    } else {
-                        pageElement.addClass('odd');
-                    }
-
-                    $(flipbookContainer).turn('addPage', pageElement);
-                });
+            page.render(renderContext).promise.then(function () {
+                console.log('PDF rendered to canvas successfully.');
+            }).catch(function (error) {
+                console.error('Error rendering PDF:', error);
             });
-        }
+        }).catch(function (error) {
+            console.error('Error getting page:', error);
+        });
+    }).catch(function (error) {
+        console.error('Error loading PDF:', error);
     });
 });
